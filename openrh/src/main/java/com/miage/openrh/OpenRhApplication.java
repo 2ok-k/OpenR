e823b4560c916824b358f632ac82af34d155fcc2
sfkd29 <58959324+sfkd29@users.noreply.github.com>
2020-10-19 00:31:01 +0000

    index adding

--- openrh/src/main/java/com/miage/openrh/OpenRhApplication.java
+++ openrh/src/main/java/com/miage/openrh/OpenRhApplication.java
@@ -2,12 +2,17 @@ package com.miage.openrh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class OpenRhApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenRhApplication.class, args);
+	}
+	@GetMapping
+	public String index(){
+		return "index";
 	}
 
 }
