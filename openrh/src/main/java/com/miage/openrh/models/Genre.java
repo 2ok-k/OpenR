5986976adb9a9a79afb9ecdae66c5665d2025aaa
2ok-k <kouassi.oumar00@gmail.com>
2020-11-13 01:42:21 +0100

    models genre

--- openrh/src/main/java/com/miage/openrh/models/Genre.java
+++ openrh/src/main/java/com/miage/openrh/models/Genre.java
@@ -1,20 +1,49 @@
 package com.miage.openrh.models;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerator;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.miage.openrh.Database;
 
-import javax.persistence.*;
-import java.util.List;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Genre {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_genre;
     private String lib_genre;
 
-   
+    public Integer getId_genre() {
+        return id_genre;
+    }
+
+    public void setId_genre(Integer id_genre) {
+        this.id_genre = id_genre;
+    }
+
+    public String getLib_genre() {
+        return lib_genre;
+    }
+
+    public void setLib_genre(String lib_genre) {
+        this.lib_genre = lib_genre;
+    }
+
+    public Genre(Integer id_genre, String lib_genre) {
+        this.id_genre = id_genre;
+        this.lib_genre = lib_genre;
+    }
+
+    public void save() throws SQLException {
+        Database db = new Database("root", "", "openrh");
+
+        db.connect();
+
+        db.sendQuery("INSERT INTO genre (id_genre,lib_genre) VALUES(?,?)", new ArrayList<>() {
+            {
+                add(id_genre);
+                add(lib_genre);
+            }
+        }, resultSet -> {
+
+        });
+
+        db.disconnect();
+    }
 }
