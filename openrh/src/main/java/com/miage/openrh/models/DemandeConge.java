de6816280030ffa249c81b76927b0d889f516ef9
2ok-k <kouassi.oumar00@gmail.com>
2020-11-13 03:27:54 +0100

    models DemandeConge

--- openrh/src/main/java/com/miage/openrh/models/DemandeConge.java
+++ openrh/src/main/java/com/miage/openrh/models/DemandeConge.java
@@ -5,121 +5,118 @@ import com.miage.openrh.Database;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class DemandeConge {
-    package com.miage.openrh.models;
-
 
 import com.miage.openrh.Database;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-    public class DemandePermission {
-        private String mat_emp;
-        private String nom;
-        private String prenom;
-        private String date_depart;
-        private String date_retour;
-        private String piece_jointe;
-        private String type_conge;
-        private String motif;
+public class DemandeConge {
+    private String mat_emp;
+    private String nom;
+    private String prenom;
+    private String date_depart;
+    private String date_retour;
+    private String piece_jointe;
+    private String type_conge;
+    private String motif;
 
-        public String getMat_emp() {
-            return mat_emp;
-        }
-
-        public void setMat_emp(String mat_emp) {
-            this.mat_emp = mat_emp;
-        }
-
-        public String getNom() {
-            return nom;
-        }
-
-        public void setNom(String nom) {
-            this.nom = nom;
-        }
-
-        public String getPrenom() {
-            return prenom;
-        }
-
-        public void setPrenom(String prenom) {
-            this.prenom = prenom;
-        }
-
-        public String getDate_depart() {
-            return date_depart;
-        }
-
-        public void setDate_depart(String date_depart) {
-            this.date_depart = date_depart;
-        }
-
-        public String getDate_retour() {
-            return date_retour;
-        }
-
-        public void setDate_retour(String date_retour) {
-            this.date_retour = date_retour;
-        }
-
-        public String getPiece_jointe() {
-            return piece_jointe;
-        }
-
-        public void setPiece_jointe(String piece_jointe) {
-            this.piece_jointe = piece_jointe;
-        }
-
-        public String getType_conge() {
-            return type_conge;
-        }
-
-        public void setType_conge(String type_conge) {
-            this.type_conge = type_conge;
-        }
-
-        public String getMotif() {
-            return motif;
-        }
-
-        public void setMotif(String motif) {
-            this.motif = motif;
-        }
-
-        public DemandePermission(String mat_emp, String nom, String prenom, String date_depart, String date_retour,String piece_jointe,String type_conge,String motif) {
-            this.mat_emp = mat_emp;
-            this.nom = nom;
-            this.prenom = prenom;
-            this.date_depart = date_depart;
-            this.date_retour = date_retour;
-            this.piece_jointe = piece_jointe;
-            this.type_conge = type_conge;
-            this.motif = motif;
-        }
-        public void save() throws SQLException {
-            Database db = new Database("root", "", "openrh");
-
-            db.connect();
-
-            db.sendQuery("INSERT INTO demandeConge(mat_emp,nom,prenom,date_depart,date_retour,piece_jointe,type_conge,motif) VALUES(?,?,?,?,?,?,?,?)", new ArrayList<>() {
-                {
-                    add(mat_emp);
-                    add(nom);
-                    add(prenom);
-                    add(date_depart);
-                    add(date_retour);
-                    add(piece_jointe);
-                    add(type_conge);
-                    add(motif);
-                }
-            }, resultSet -> {
-
-            });
-
-            db.disconnect();
-        }
+    public String getMat_emp() {
+        return mat_emp;
     }
 
+    public void setMat_emp(String mat_emp) {
+        this.mat_emp = mat_emp;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getDate_depart() {
+        return date_depart;
+    }
+
+    public void setDate_depart(String date_depart) {
+        this.date_depart = date_depart;
+    }
+
+    public String getDate_retour() {
+        return date_retour;
+    }
+
+    public void setDate_retour(String date_retour) {
+        this.date_retour = date_retour;
+    }
+
+    public String getPiece_jointe() {
+        return piece_jointe;
+    }
+
+    public void setPiece_jointe(String piece_jointe) {
+        this.piece_jointe = piece_jointe;
+    }
+
+    public String getType_conge() {
+        return type_conge;
+    }
+
+    public void setType_conge(String type_conge) {
+        this.type_conge = type_conge;
+    }
+
+    public String getMotif() {
+        return motif;
+    }
+
+    public void setMotif(String motif) {
+        this.motif = motif;
+    }
+
+    public DemandeConge(String mat_emp, String nom, String prenom, String date_depart, String date_retour,String piece_jointe,String type_conge,String motif) {
+        this.mat_emp = mat_emp;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.date_depart = date_depart;
+        this.date_retour = date_retour;
+        this.piece_jointe = piece_jointe;
+        this.type_conge = type_conge;
+        this.motif = motif;
+    }
+    public void save() throws SQLException {
+        Database db = new Database("root", "", "openrh");
+
+        db.connect();
+
+        db.sendQuery("INSERT INTO demandeConge(mat_emp,nom,prenom,date_depart,date_retour,piece_jointe,type_conge,motif) VALUES(?,?,?,?,?,?,?,?)", new ArrayList<>() {
+            {
+                add(mat_emp);
+                add(nom);
+                add(prenom);
+                add(date_depart);
+                add(date_retour);
+                add(piece_jointe);
+                add(type_conge);
+                add(motif);
+            }
+        }, resultSet -> {
+
+        });
+
+        db.disconnect();
+    }
 }
+
+
