6efa3d4581b05ffefa4f6879df87142c6686bc27
2ok-k <kouassi.oumar00@gmail.com>
2020-11-13 02:30:19 +0100

    models typeContrat

---
+++ openrh/src/main/java/com/miage/openrh/models/Structure.java
@@ -0,0 +1,48 @@
+package com.miage.openrh.models;
+
+import com.miage.openrh.Database;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Structure {
+    private Integer cod_struct;
+    private String lib_struct;
+
+    public Integer getCod_struct() {
+        return cod_struct;
+    }
+
+    public void setCod_struct(Integer cod_struct) {
+        this.cod_struct = cod_struct;
+    }
+
+    public String getLib_struct() {
+        return lib_struct;
+    }
+
+    public void setLib_struct(String lib_struct) {
+        this.lib_struct = lib_struct;
+    }
+
+    public Structure(Integer cod_struct, String lib_struct) {
+        this.cod_struct = cod_struct;
+        this.lib_struct = lib_struct;
+    }
+    public void save() throws SQLException {
+        Database db = new Database("root", "", "openrh");
+
+        db.connect();
+
+        db.sendQuery("INSERT INTO structure(cod_struct,lib_struct) VALUES(?,?)", new ArrayList<>() {
+            {
+                add(cod_struct);
+                add(lib_struct);
+            }
+        }, resultSet -> {
+
+        });
+
+        db.disconnect();
+    }
+}
--- openrh/src/main/java/com/miage/openrh/models/TypeContrat.java
+++ openrh/src/main/java/com/miage/openrh/models/TypeContrat.java
@@ -1,24 +1,49 @@
 package com.miage.openrh.models;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.miage.openrh.Database;
 
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id_type_contrat")
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class TypeContrat {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_type_contrat;
     private String lib_contrat;
+
+    public Integer getId_type_contrat() {
+        return id_type_contrat;
+    }
+
+    public void setId_type_contrat(Integer id_type_contrat) {
+        this.id_type_contrat = id_type_contrat;
+    }
+
+    public String getLib_contrat() {
+        return lib_contrat;
+    }
+
+    public void setLib_contrat(String lib_contrat) {
+        this.lib_contrat = lib_contrat;
+    }
+
+    public TypeContrat(Integer id_type_contrat, String lib_contrat) {
+        this.id_type_contrat = id_type_contrat;
+        this.lib_contrat = lib_contrat;
+    }
+    public void save() throws SQLException {
+        Database db = new Database("root", "", "openrh");
+
+        db.connect();
+
+        db.sendQuery("INSERT INTO typeContrat(id_type_contrat,lib_contrat) VALUES(?,?)", new ArrayList<>() {
+            {
+                add(id_type_contrat);
+                add(lib_contrat);
+            }
+        }, resultSet -> {
+
+        });
+
+        db.disconnect();
+    }
 }
