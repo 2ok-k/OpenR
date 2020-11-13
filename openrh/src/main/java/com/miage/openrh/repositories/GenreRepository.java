a66175585b264ac21a43c5c11eafe4c8dd953aad
sfkd29 <58959324+sfkd29@users.noreply.github.com>
2020-11-13 00:23:28 +0000

    update projects

--- openrh/src/main/java/com/miage/openrh/repositories/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.miage.openrh.repositories;
-
-import com.miage.openrh.models.Conge;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenreRepository extends JpaRepository<Conge, Integer> {
-}
--- openrh/src/main/java/com/miage/openrh/services/TypeCongeService.java
+++
@@ -1,7 +0,0 @@
-package com.miage.openrh.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class TypeCongeService {
-}
