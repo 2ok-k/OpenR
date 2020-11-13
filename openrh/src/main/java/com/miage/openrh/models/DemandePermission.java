893fc8fa1aeb4e9f73af6a2f1f035fd6743a7b7f
2ok-k <kouassi.oumar00@gmail.com>
2020-11-13 03:22:50 +0100

    models DemandeConge

--- openrh/src/main/java/com/miage/openrh/models/DemandePermission.java
+++ openrh/src/main/java/com/miage/openrh/models/DemandePermission.java
@@ -75,7 +75,7 @@ public class DemandePermission {
 
         db.connect();
 
-        db.sendQuery("INSERT INTO DemandePermission(mat_emp,nom,prenom,date_depart,date_retour,motif) VALUES(?,?,?,?,?,?)", new ArrayList<>() {
+        db.sendQuery("INSERT INTO demandePermission(mat_emp,nom,prenom,date_depart,date_retour,motif) VALUES(?,?,?,?,?,?)", new ArrayList<>() {
             {
                 add(mat_emp);
                 add(nom);
