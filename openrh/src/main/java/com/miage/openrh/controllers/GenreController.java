ca94cdbbf83463fa136fa309995e3887fefae16a
2ok-k <kouassi.oumar00@gmail.com>
2020-11-14 04:45:39 +0000

    insert genre

--- openrh/src/main/java/com/miage/openrh/controllers/GenreController.java
+++ openrh/src/main/java/com/miage/openrh/controllers/GenreController.java
@@ -1,6 +1,8 @@
 package com.miage.openrh.controllers;
 
+import com.miage.openrh.Database;
 import com.miage.openrh.models.Genre;
+import com.miage.openrh.models.Pays;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class GenreController {
 
@@ -26,6 +31,22 @@ public class GenreController {
     @GetMapping(value ="/genre")
     public String genre(Model model){
         Genre genre = new Genre();
+        List<Genre> genres=new ArrayList<>();
+        Database db =new Database("root", "","openrh");
+        db.connect();
+
+        try {
+            db.sendQuery("SELECT * FROM genre",resultSet -> {
+                while(resultSet.next()){
+                    genres.add(new Genre(resultSet.getInt("id_genre"),resultSet.getString("lib_genre")));
+                }
+            });
+
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        model.addAttribute("genres",genres);
 
         model.addAttribute("Genre",genre);
 
