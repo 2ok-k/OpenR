f565a345bae157ed6408fef57f0a66a21ec3d9d1
2ok-k <kouassi.oumar00@gmail.com>
2020-11-14 12:03:27 +0000

    insert structure

--- openrh/src/main/java/com/miage/openrh/controllers/StructureController.java
+++ openrh/src/main/java/com/miage/openrh/controllers/StructureController.java
@@ -1,5 +1,7 @@
 package com.miage.openrh.controllers;
 
+import com.miage.openrh.Database;
+import com.miage.openrh.models.Genre;
 import com.miage.openrh.models.Structure;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class StructureController {
@@ -26,6 +30,22 @@ public class StructureController {
     @GetMapping(value ="/structure")
     public String structure(Model model){
         Structure structure = new Structure();
+        List<Structure> structures=new ArrayList<>();
+        Database db =new Database("root", "","openrh");
+        db.connect();
+
+        try {
+            db.sendQuery("SELECT * FROM structure",resultSet -> {
+                while(resultSet.next()){
+                    structures.add(new Structure(resultSet.getInt("cod_struct"),resultSet.getString("lib_struct")));
+                }
+            });
+
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        model.addAttribute("structures",structures);
 
         model.addAttribute("Structure",structure);
 
