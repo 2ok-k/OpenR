68665a6965292234fa8b1e90aa6afebd60e64c4a
2ok-k <kouassi.oumar00@gmail.com>
2020-11-14 11:46:38 +0000

    insert situation

--- openrh/src/main/java/com/miage/openrh/controllers/SituationMatriController.java
+++ openrh/src/main/java/com/miage/openrh/controllers/SituationMatriController.java
@@ -1,5 +1,6 @@
 package com.miage.openrh.controllers;
 
+import com.miage.openrh.Database;
 import com.miage.openrh.models.SituationMatri;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class SituationMatriController {
@@ -26,6 +29,22 @@ public class SituationMatriController {
     @GetMapping(value ="/situationMatri")
     public String situationMatri(Model model){
         SituationMatri situationMatri = new SituationMatri();
+        List<SituationMatri> situationMatris=new ArrayList<>();
+        Database db =new Database("root", "","openrh");
+        db.connect();
+
+        try {
+            db.sendQuery("SELECT * FROM situationmatri",resultSet -> {
+                while(resultSet.next()){
+                    situationMatris.add(new SituationMatri(resultSet.getInt("id_sit"),resultSet.getString("lib_sit")));
+                }
+            });
+
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        model.addAttribute("situationMatris",situationMatris);
 
         model.addAttribute("SituationMatri",situationMatri);
 
