f4d96413ed19789b0cab313083fc26373b7a335b
2ok-k <kouassi.oumar00@gmail.com>
2020-11-14 12:57:50 +0000

    insert secteur,grpUsers

--- openrh/src/main/java/com/miage/openrh/controllers/GroupeUtilisateurController.java
+++ openrh/src/main/java/com/miage/openrh/controllers/GroupeUtilisateurController.java
@@ -1,7 +1,9 @@
 package com.miage.openrh.controllers;
 
+import com.miage.openrh.Database;
 import com.miage.openrh.models.GroupeUtilisateur;
 import com.miage.openrh.models.SituationMatri;
+import com.miage.openrh.models.TypeContrat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GroupeUtilisateurController {
@@ -27,6 +31,22 @@ public class GroupeUtilisateurController {
     @GetMapping(value ="/groupeUtilisateur")
     public String groupeUtilisateur(Model model){
         GroupeUtilisateur groupeUtilisateur = new GroupeUtilisateur();
+        List<GroupeUtilisateur> groupeUtilisateurs=new ArrayList<>();
+        Database db =new Database("root", "","openrh");
+        db.connect();
+
+        try {
+            db.sendQuery("SELECT * FROM groupeutilisateur",resultSet -> {
+                while(resultSet.next()){
+                    groupeUtilisateurs.add(new GroupeUtilisateur(resultSet.getInt("id_grpUt"),resultSet.getString("lib_grpUt")));
+                }
+            });
+
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        model.addAttribute("groupeUtilisateurs",groupeUtilisateurs);
 
         model.addAttribute("GroupeUtilisateur",groupeUtilisateur);
 
--- openrh/src/main/java/com/miage/openrh/controllers/SecteurController.java
+++ openrh/src/main/java/com/miage/openrh/controllers/SecteurController.java
@@ -1,6 +1,8 @@
 package com.miage.openrh.controllers;
 
+import com.miage.openrh.Database;
 import com.miage.openrh.models.Secteur;
+import com.miage.openrh.models.TypeContrat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class SecteurController {
     @PostMapping(value = "/enregistrersecteur")
@@ -25,6 +30,22 @@ public class SecteurController {
     @GetMapping(value ="/secteur")
     public String secteur(Model model){
         Secteur secteur = new Secteur();
+        List<Secteur> secteurs=new ArrayList<>();
+        Database db =new Database("root", "","openrh");
+        db.connect();
+
+        try {
+            db.sendQuery("SELECT * FROM secteur",resultSet -> {
+                while(resultSet.next()){
+                    secteurs.add(new Secteur(resultSet.getInt("id_sect"),resultSet.getString("lib_sect")));
+                }
+            });
+
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        model.addAttribute("secteurs",secteurs);
 
         model.addAttribute("Secteur",secteur);
 
