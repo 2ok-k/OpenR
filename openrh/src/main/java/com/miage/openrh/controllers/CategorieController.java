17c9429005a09b8967d0c90fe8a65e44381a8b7e
2ok-k <kouassi.oumar00@gmail.com>
2020-11-14 12:36:48 +0000

    insert categorie

--- openrh/src/main/java/com/miage/openrh/controllers/CategorieController.java
+++ openrh/src/main/java/com/miage/openrh/controllers/CategorieController.java
@@ -1,7 +1,9 @@
 package com.miage.openrh.controllers;
 
+import com.miage.openrh.Database;
 import com.miage.openrh.models.Categorie;
 import com.miage.openrh.models.Entreprise;
+import com.miage.openrh.models.TypeContrat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CategorieController {
@@ -27,6 +31,22 @@ public class CategorieController {
     @GetMapping(value ="/categorie")
     public String categorie(Model model){
         Categorie categorie = new Categorie();
+        List<Categorie> categories=new ArrayList<>();
+        Database db =new Database("root", "","openrh");
+        db.connect();
+
+        try {
+            db.sendQuery("SELECT * FROM categorie",resultSet -> {
+                while(resultSet.next()){
+                    categories.add(new Categorie(resultSet.getInt("id_cat"),resultSet.getString("lib_cat")));
+                }
+            });
+
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        model.addAttribute("categories",categories);
 
         model.addAttribute("Categorie",categorie);
 
