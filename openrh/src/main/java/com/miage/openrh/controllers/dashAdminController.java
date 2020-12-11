0481277d4af43f078468b3863ee20114d90103a1
2ok-k <kouassi.oumar00@gmail.com>
2020-12-11 12:22:43 +0000

    update

--- openrh/src/main/java/com/miage/openrh/controllers/dashAdminController.java
+++ openrh/src/main/java/com/miage/openrh/controllers/dashAdminController.java
@@ -1,12 +1,36 @@
 package com.miage.openrh.controllers;
 
+import com.miage.openrh.Database;
+import com.miage.openrh.models.AjoutEmploye;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class dashAdminController {
     @GetMapping(value ="/dashAdmin")
-    public String dashAdmin(){
+    public String dashAdmin(Model model){
+        List<AjoutEmploye> ajoutEmployes=new ArrayList<>();
+
+        Database db =new Database("root", "","openrh");
+        db.connect();
+        try {
+            db.sendQuery("SELECT * FROM ajoutemploye",resultSet -> {
+                while(resultSet.next()){
+                    ajoutEmployes.add(new AjoutEmploye(resultSet.getString("mat_emp"),resultSet.getString("nom"),resultSet.getString("prenom"),resultSet.getString("genre"),resultSet.getString("date_naiss"),resultSet.getString("photo"),resultSet.getString("lieu_naiss"),resultSet.getString("email"),resultSet.getString("numero"),resultSet.getString("situationMatri"),resultSet.getString("poste"),resultSet.getString("salaire")));
+                }
+            });
+
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+
+        model.addAttribute("ajoutEmployes",ajoutEmployes);
+
         return "dashAdmin";
     }
 }
