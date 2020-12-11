e6c917ad2cb38eabbb13595cd779d571f3897256
2ok-k <kouassi.oumar00@gmail.com>
2020-12-11 16:28:07 +0000

    update

--- openrh/src/main/java/com/miage/openrh/models/AjoutEmploye.java
+++ openrh/src/main/java/com/miage/openrh/models/AjoutEmploye.java
@@ -2,11 +2,33 @@ package com.miage.openrh.models;
 
 import com.miage.openrh.Database;
 
+import java.nio.charset.Charset;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class AjoutEmploye {
-    private String mat_emp;
+
+    public String givenUsingPlainJava_whenGeneratingRandomStringBounded_thenCorrect() {
+
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 5;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+
+        System.out.println(generatedString);
+        return givenUsingPlainJava_whenGeneratingRandomStringBounded_thenCorrect();
+    }
+
+
+    private String mat_emp = givenUsingPlainJava_whenGeneratingRandomStringBounded_thenCorrect();
     private String nom;
     private String prenom;
     private String genre;
