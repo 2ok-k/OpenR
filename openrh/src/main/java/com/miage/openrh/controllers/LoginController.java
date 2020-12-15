066e1ed8dd56bacfdfc9b4b4447b6ee962b5eaca
sfkd29 <58959324+sfkd29@users.noreply.github.com>
2020-12-15 02:59:10 +0000

    Update LoginController.java

--- openrh/src/main/java/com/miage/openrh/controllers/LoginController.java
+++ openrh/src/main/java/com/miage/openrh/controllers/LoginController.java
@@ -1,8 +1,11 @@
 package com.miage.openrh.controllers;
 
+import com.miage.openrh.models.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -10,6 +13,17 @@ public class LoginController {
 
     @GetMapping(value ="/login")
     public String login(Model model){
+        model.addAttribute("users",new Users());
         return "login";
+    }
+
+    @PostMapping("/verify")
+    String pageAppropriee(@ModelAttribute("users") Users users) {
+        if (users.exist()) {
+
+            return "dashAdmin";
+        } else {
+            return "login";
+        }
     }
 }
