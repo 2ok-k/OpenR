0c0cc893c13e7c9ca8bbd0d8aa796c65b1394e0a
sfkd29 <58959324+sfkd29@users.noreply.github.com>
2020-12-15 03:25:16 +0000

    Update LoginController.java

--- openrh/src/main/java/com/miage/openrh/controllers/LoginController.java
+++ openrh/src/main/java/com/miage/openrh/controllers/LoginController.java
@@ -13,7 +13,7 @@ public class LoginController {
 
     @GetMapping(value ="/login")
     public String login(Model model){
-        model.addAttribute("user",new Users());
+        model.addAttribute("users",new Users());
         return "login";
     }
 
