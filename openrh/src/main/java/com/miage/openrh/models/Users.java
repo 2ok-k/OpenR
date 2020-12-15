a5580238e2376018c86e080ae8d7e161b760d131
2ok-k <kouassi.oumar00@gmail.com>
2020-12-15 02:48:42 +0000

    update

---
+++ openrh/src/main/java/com/miage/openrh/models/Users.java
@@ -0,0 +1,60 @@
+package com.miage.openrh.models;
+
+import com.miage.openrh.Database;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Users {
+    private String groupe_util;
+    private String matricule;
+    private String password;
+
+    public String getGroupe_util() {
+        return groupe_util;
+    }
+
+    public void setGroupe_util(String groupe_util) {
+        this.groupe_util = groupe_util;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Users(String groupe_util, String matricule, String password) {
+        this.groupe_util = groupe_util;
+        this.matricule = matricule;
+        this.password = password;
+    }
+
+    public void save() throws SQLException {
+        Database db = new Database("root", "", "openrh");
+
+        db.connect();
+
+        db.sendQuery("INSERT INTO users(groupe_util,matricule,password) VALUES(?,?,?)", new ArrayList<>() {
+            {
+                add(groupe_util);
+                add(matricule);
+                add(password);
+            }
+        }, resultSet -> {
+
+        });
+
+        db.disconnect();
+    }
+}
